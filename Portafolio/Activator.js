583260c54ec6bcb8b846402ee77dbbf1ea150a07
Bc219132 <bc219132@Bancaribe.com.ve>
2021-11-10 16:52:24 -0400

    Update Activator.js

--- Portafolio/Activator.js
+++ Portafolio/Activator.js
@@ -39,19 +39,38 @@ window.addEventListener('load', () => {
 
 //Login y Contraseña 
 
+//document.getElementById("Inicio_Sesión").addEventListener("click", register);
+
+// Variables 
+//var Contenedor_IniciarRegistrar = document.querySelector(".Contenedor_IniciarRegistrar");
+//var Iniciar_Sesión = document.querySelector(".Iniciar_Sesión");
+//var Iniciar_Registro = document.querySelector(".Iniciar_Registro");
+//var Login = document.querySelector(".Login");
+//var Registro = document.querySelector(".Registro");
+
+//function register(){
+//	Iniciar_Registro.style.display = "block";
+//	Contenedor_IniciarRegistrar.style.left = "410px";
+//	Iniciar_Sesión.style.display = "none";
+//	Registro.style.opacity = "0";
+//	Login.style.opacity = "1";
+//}
+
+// 
+
 document.getElementById("Inicio_Sesión").addEventListener("click", register);
 
-// Variables
-var Contenedor_IniciarRegistrar = document.querySelector(".Contenedor_IniciarRegistrar");
-var Iniciar_Sesión = document.querySelector(".Iniciar_Sesión");
-var Iniciar_Registro = document.querySelector(".Iniciar_Registro");
-var Login = document.querySelector(".Login");
-var Registro = document.querySelector(".Registro");
+//Declarando variables
+var formulario_login = document.querySelector(".Contenedor_IniciarRegistrar");
+var formulario_register = document.querySelector(".Iniciar_Sesión");
+var contenedor_login_register = document.querySelector(".Iniciar_Registro");
+var caja_trasera_login = document.querySelector(".Login");
+var caja_trasera_register = document.querySelector(".Registro");
 
 function register(){
-	Iniciar_Registro.style.display = "block";
-	Contenedor_IniciarRegistrar.style.left = "410px";
-	Iniciar_Sesión.style.display = "none";
-	Registro.style.opacity = "0";
-	Login.style.opacity = "1";
-}
+		formulario_register.style.display = "block";
+		contenedor_login_register.style.left = "410px";
+		formulario_login.style.display = "none";
+		caja_trasera_register.style.opacity = "0";
+		caja_trasera_login.style.opacity = "1";
+	}
